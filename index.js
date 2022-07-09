5e85b304081689db0246453ca8e4b256af6b2cb0
Christopher Sarmiento <chrisar11@gmail.com>
2022-07-09 16:35:25 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(name, event) {
+    let thankYou = [];
+    for (let i = 0; i < name.length; i++) {
+        thankYou.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYou;
+}
+
+function countDown(integer) {
+    let i = 10;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
